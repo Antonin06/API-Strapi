f2c8d5c39f150989bfb6771f9bea800cfe82307a
Antonin06 <avon.antonin@gmai.com>
2021-11-08 21:26:46 +0100

    first commit

---
+++ api/book/controllers/book.js
@@ -0,0 +1,18 @@
+const { sanitizeEntity } = require('strapi-utils');
+
+module.exports = {
+  async comment(ctx) {
+    let entity;
+      ctx.request.body.authorComment = ctx.state.user.id;
+      ctx.request.body.book = ctx.params.id;
+      entity = await strapi.services.comment.create(ctx.request.body);
+    return sanitizeEntity(entity, { model: strapi.models.comment });
+  },
+  async favorite(ctx) {
+    let entity;
+    ctx.request.body.authorFavorite = ctx.state.user.id;
+    ctx.request.body.book = ctx.params.id;
+    entity = await strapi.services.favorite.create(ctx.request.body);
+    return sanitizeEntity(entity, { model: strapi.models.favorite });
+  },
+};
